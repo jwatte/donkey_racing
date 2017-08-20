2c810f8d0c4f488433506fcbb014513288fe47ca
Jon Watte <jwatte@gmail.com>
2017-08-19 23:24:56 -0700

    improve the RC control; use serial channel data

--- teensy_rc_control/teensy_rc_control.ino
+++ teensy_rc_control/teensy_rc_control.ino
@@ -1,84 +1,165 @@
-#include "PinPulseIn.h"
 #include "Calibrate.h"
+#include "PinPulseIn.h"
+#include "FlySkyIBus.h"
+
 #include <Servo.h>
+#include <EEPROM.h>
 
 
 #define CENTER_CALIBRATION 1540
 
 PinPulseIn<14> rcSteer;
-PinPulseIn<15> rcThrottle;
 
 Servo carSteer;
 Servo carThrottle;
+
+uint16_t fsValues[10];
+FlySkyIBus iBus(Serial1, fsValues, 10);
 
 Calibrate inSteer;
 Calibrate inThrottle;
 Calibrate outSteer(CENTER_CALIBRATION);
 Calibrate outThrottle;
 
-int16_t readSteer = 0;
-int16_t readThrottle = 0;
-
 int16_t sendSteer = 0;
 int16_t sendThrottle = 0;
 
-bool driving = false;
+bool hasRC = false;
 uint32_t lastModeTime = 0;
+
+
+void readTrim();
+void writeTrim();
+void maybeTrim(uint32_t now);
 
 
 void setup() {
   rcSteer.init();
-  rcThrottle.init();
-  carSteer.attach(23);
-  carThrottle.attach(22);
+  iBus.begin();
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(13, OUTPUT);
+  readTrim();
 }
 
 
 void loop() {
 
   uint32_t now = millis();
-  
-  if (rcThrottle.hasValue()) {
-    readThrottle = rcThrottle.getValue();
-    sendThrottle = outThrottle.mapOut(inThrottle.mapIn(readThrottle));
-    lastModeTime = now;
-    driving = true;
-  }
+
+  //  I only use PinPulseIn to detect that there is a carrier
   if (rcSteer.hasValue()) {
-    readSteer = rcSteer.getValue();
-    sendSteer = outSteer.mapOut(inSteer.mapIn(readSteer));
     lastModeTime = now;
-    driving = true;
+    hasRC = true;
   }
 
   if (now - lastModeTime > 300) {
     lastModeTime = now - 300;
-    driving = false;
+    hasRC = false;
   }
 
-  if (!driving) {
+  if (hasRC) {
+    maybeTrim(now);
+  }
+  
+  if (!hasRC || fsValues[9] < 1750) {
     /* turn off the servos */
-    carSteer.writeMicroseconds(outSteer.center_);
-    carThrottle.writeMicroseconds(outThrottle.center_);
+    if (carSteer.attached()) {
+      carSteer.detach();
+    }
+    if (carThrottle.attached()) {
+      carThrottle.detach();
+    }
     digitalWrite(13, (now & 192) == 64 );
     digitalWrite(3, (now & 192) == 128);
     digitalWrite(4, (now & 192) == 192);
   } else {
     /* send the current heading/speed */
+    if (!carSteer.attached()) {
+      carSteer.attach(23);
+    }
+    if (!carThrottle.attached()) {
+      carThrottle.attach(22);
+    }
+    if (iBus.hasFreshFrame()) {
+      sendSteer = outSteer.mapOut(inSteer.mapIn(fsValues[0]));
+      sendThrottle = outThrottle.mapOut(inThrottle.mapIn(fsValues[1]));
+    }
     carSteer.writeMicroseconds(sendSteer);
     carThrottle.writeMicroseconds(sendThrottle);
-    digitalWrite(13, driving ? HIGH : LOW);
+    digitalWrite(13, sendThrottle > outThrottle.center_ ? HIGH : LOW);
     digitalWrite(3, sendSteer < outSteer.center_ ? HIGH : LOW);
     digitalWrite(4, sendSteer > outSteer.center_ ? HIGH : LOW);
   }
 
   do {
-    // usbCommand.readSome();
-    // ser3Command.readSome();
+    iBus.update();
   } while (millis() < now + 5);
 }
 
+uint32_t lastTrimTime;
+uint32_t trimmedTime;
+
+void maybeTrim(uint32_t now) {
+
+  if (now - lastTrimTime >= 100) {
+  
+    lastTrimTime = now;
+
+    //  trim enable switch
+    if (fsValues[6] < 1700) {
+      return;
+    }
+    
+    /* trim steering */
+    if (fsValues[5] > 1700) {
+      if (outSteer.center_ > 1300) {
+        outSteer.center_ -= 1;
+        trimmedTime = now;
+      }
+    } else if (fsValues[5] < 1300) {
+      if (outSteer.center_ < 1700) {
+        outSteer.center_ += 1;
+        trimmedTime = now;
+      }
+    }
+
+    /* trim throttle */
+    if (fsValues[4] > 1700) {
+      if (outThrottle.center_ > 1300) {
+        outThrottle.center_ -= 1;
+        trimmedTime = now;
+      }
+    } else if (fsValues[4] < 1500) {
+      if (outThrottle.center_ < 1700) {
+        outThrottle.center_ += 1;
+        trimmedTime = now;
+      }
+    }
+
+    if ((trimmedTime != 0) && (now - trimmedTime > 10000)) {
+      //  write to EEPROM 10 seconds after a trim has been made and no further changes
+      writeTrim();
+      trimmedTime = 0;
+    }
+  }
+}
+
+void readTrim() {
+  uint16_t center = EEPROM.read(2) + ((uint16_t)EEPROM.read(3) << 8);
+  if (center >= 1300 && center <= 1700) {
+    outSteer.center_ = center;
+  }
+  center = EEPROM.read(4) + ((uint16_t)EEPROM.read(5) << 8);
+  if (center >= 1300 && center <= 1700) {
+    outThrottle.center_ = center;
+  }
+}
+
+void writeTrim() {
+  EEPROM.update(2, outSteer.center_ & 0xff);
+  EEPROM.update(3, (outSteer.center_ & 0xff00) >> 8);
+  EEPROM.update(4, outThrottle.center_ & 0xff);
+  EEPROM.update(5, (outThrottle.center_ & 0xff00) >> 8);
+}
 
