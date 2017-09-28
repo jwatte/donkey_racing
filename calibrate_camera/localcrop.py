9ee796ddb6e01ea9611d57ad1c84d9f9036d33fe
Jon Watte <jwatte@gmail.com>
2017-09-28 16:19:02 -0700

    use a larger area of input data

--- calibrate_camera/localcrop.py
+++ calibrate_camera/localcrop.py
@@ -1,9 +1,9 @@
 
 x = 50
-y = 63
-w = 640-2*x # 448, div3 = 149
-h = 480-2*y # 354, div2 = 177, div3 = 59
-yoffset = -60 # change this if you want to crop higher up or lower down
+y = 60
+w = 640-2*x
+h = 480-2*y
+yoffset = -57 # change this if you want to crop higher up or lower down
             # more positive number means further down
 
 def params():
--- calibrate_camera/table.h
+++ calibrate_camera/table.h
@@ -5,19 +5,19 @@
 
 #define SOURCE_WIDTH 640
 #define SOURCE_HEIGHT 480
-#define INPUT_LEFT 96
+#define INPUT_LEFT 50
 #define INPUT_TOP 240
-#define INPUT_WIDTH 448
-#define INPUT_HEIGHT 177
-#define RECTIFIED_WIDTH 149
-#define RECTIFIED_HEIGHT 59
+#define INPUT_WIDTH 540
+#define INPUT_HEIGHT 180
+#define RECTIFIED_WIDTH 180
+#define RECTIFIED_HEIGHT 60
 
 typedef struct TableInputCoord {
   float sy;
   float sx;
 } TableInputCoord;
 
-/* total table size 8791 elements 70328 bytes */
+/* total table size 10800 elements 86400 bytes */
 extern TableInputCoord const sTableInputCoords[RECTIFIED_HEIGHT][RECTIFIED_WIDTH];
 
 #endif
