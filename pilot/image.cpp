2e9786d9bdaa7741a6c23845416933c89094ddb8
Jon Watte <jwatte@gmail.com>
2017-10-21 17:53:11 -0700

    make image more sensitive to yellow

--- pilot/image.cpp
+++ pilot/image.cpp
@@ -18,6 +18,36 @@ void get_unwarp_info(size_t *osize, int *owidth, int *oheight, int *oplanes) {
 static const float BYTE_TO_FLOAT = 1.0f / 255.0f;
 static const float FLOAT_TO_BYTE = 255.0f;
 
+static inline float munge(int i) {
+    float f = (((float)i) - 20) * 0.9f;
+    return (f < 0) ? 0 : (f > 255) ? 255 : f;
+}
+
+static inline float munge(float f) {
+    return (f < 0) ? 0 : (f > 255) ? 255 : f;
+}
+
+static inline float sample_yuv(unsigned char const *y, unsigned char const *u, unsigned char const *v, TableInputCoord const tic) {
+    float y1f = floorf(tic.sy);
+    float y1d = tic.sy - y1f;
+    int yc = (int)y1f + 15;
+    int x1f = floorf(tic.sx);
+    float x1d = tic.sx - x1f;
+    int xc = (int)x1f;
+    y += yc * SOURCE_WIDTH + xc;
+    int a = y[0];
+    int b = y[1];
+    int c = y[SOURCE_WIDTH];
+    int d = y[SOURCE_WIDTH+1];
+    float yy = ((a * (1.0f - x1d) + b * x1d) * (1 - y1d) +
+            (c * (1.0f - x1d) + d * x1d) * y1d);
+    int uy = yc / 2;
+    int ux = xc / 2;
+    float uu = u[uy * (SOURCE_WIDTH/2) + ux] + 12;
+    float vv = v[ux] - 35;
+    return munge(255 + yy*1.5f - (fabsf(yy-200) + fabsf(uu) + fabsf(vv)) * 0.65f) * BYTE_TO_FLOAT;
+}
+
 static inline float sample_y(unsigned char const *y, TableInputCoord const tic) {
     float y1f = floorf(tic.sy);
     float y1d = tic.sy - y1f;
@@ -30,18 +60,20 @@ static inline float sample_y(unsigned char const *y, TableInputCoord const tic) 
     int b = y[1];
     int c = y[SOURCE_WIDTH];
     int d = y[SOURCE_WIDTH+1];
-    return ((a * (1.0f - x1d) + b * x1d) * (1 - y1d) +
-            (c * (1.0f - x1d) + d * x1d) * y1d) * BYTE_TO_FLOAT;
+    return munge(((a * (1.0f - x1d) + b * x1d) * (1 - y1d) +
+            (c * (1.0f - x1d) + d * x1d) * y1d)) * BYTE_TO_FLOAT;
 }
 
 void unwarp_image(void const *src, void *dst) {
     unsigned char const *y = (unsigned char const *)src;
+    unsigned char const *u = y + SOURCE_WIDTH * SOURCE_HEIGHT;
+    unsigned char const *v = u + (SOURCE_WIDTH/2) * (SOURCE_HEIGHT/2);
 
     float *dp1 = (float *)dst;
     TableInputCoord const *ticp = &sTableInputCoords[0][0];
     for (int yy = 0; yy < RECTIFIED_HEIGHT; ++yy) {
         for (int xx = 0; xx < RECTIFIED_WIDTH; ++xx) {
-            float dp1v = sample_y(y, *ticp);
+            float dp1v = sample_yuv(y, u, v, *ticp);
             *dp1++ = dp1v;
             ++ticp;
         }
