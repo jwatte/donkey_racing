788f36216d80d01afcd815b3fdd83cdb9cc521b2
Jon Watte <jwatte@gmail.com>
2017-11-01 20:30:03 -0700

    try to make flat map

--- calibrate_camera/localcrop.py
+++ calibrate_camera/localcrop.py
@@ -10,16 +10,51 @@ yoffset = -60 # change this if you want to crop higher up or lower down
 def params():
     return (x, y, w, h, yoffset)
 
-s_down=20
-s_near=150
-s_hfovy=45
-s_cy=240
-s_hwidth=270
-s_hfovx=60
-s_cx=320
+def deg(r):
+    return r * 180.0 / math.pi
 
-def squareParams():
-    return (s_down, s_near, s_hfovy, s_cy, s_hwidth, s_fovx, s_cx)
+def rad(d):
+    return d * math.pi / 180.0
 
-def squareMap(dx, dy):
-    
+s_width=540
+s_left=(640-s_width)/2
+s_height=220
+s_topcrop=54
+s_bottom=480-(480-s_height-s_topcrop)/2
+s_centerangle=22.5
+s_camheight=26.0
+s_bottomfront=10.0
+s_bottomangle=90.0-s_centerangle-deg(math.atan(s_bottomfront/s_camheight))
+
+s_centertobottom=137.0
+s_tanbottomangle=math.tan(rad(s_bottomangle))
+s_planefront=s_centertobottom/s_tanbottomangle
+
+
+def _sq_ypos(f):
+    f_angle=deg(math.atan(f/s_camheight))
+    f_relangle=90.0-f_angle-s_centerangle
+    f_tana=math.tan(rad(f_relangle))
+    return f_tana
+
+def sq_ypos(f):
+    f_tana=_sq_ypos(f)
+    return s_planefront*f_tana+s_bottom-s_centertobottom
+
+def sq_xpos(f,x):
+    f_fdist=math.sqrt(s_camheight*s_camheight+f*f)
+    f_side=s_planefront*x/f_fdist
+    f_ret=f_side+s_left+s_width/2
+    f_ok=(f_ret>=s_left and f_ret<=(s_left+s_width))
+    return f_ret, f_ok
+
+if __name__ == "__main__":
+    y=sq_ypos(62.7)
+    x,_=sq_xpos(62.7,0.0)
+    print("62.7,0.0="+str(y)+","+str(x))
+    y=sq_ypos(10.0)
+    x,_=sq_xpos(10.0,-30)
+    print("10.0,-30.0="+str(y)+","+str(x))
+    y=sq_ypos(100.0)
+    x,_=sq_xpos(100.0,30.0)
+    print("100.0,30.0="+str(y)+","+str(x))
