b17e631cdb04f1aa7f46ccac3c1669cc86d3f3af
Jon Watte <jwatte@gmail.com>
2018-01-22 22:02:19 -0800

    smaller speed range on test

--- test_pca.py
+++ test_pca.py
@@ -7,9 +7,9 @@ pwm.set_pwm_freq(60)
 while True:
   for i in range(300, 400):
     pwm.set_pwm(0, 0, i)
-    pwm.set_pwm(1, 0, 380)
+    pwm.set_pwm(1, 0, 352+((i-300)/3))
     time.sleep(0.05)
   for i in range(400, 300, -1):
     pwm.set_pwm(0, 0, i)
-    pwm.set_pwm(1, 0, 390)
+    pwm.set_pwm(1, 0, 352+((i-300)/3))
     time.sleep(0.05)
