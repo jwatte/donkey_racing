9ff266577664e72e8d1b3d410387188d536727b8
Jon Watte <jwatte@gmail.com>
2018-03-15 22:08:18 -0700

    Initial bang-out of VESC UART commands.

---
+++ teensy_hat_firmware/VescCommands.cpp
@@ -0,0 +1,223 @@
+#include "VescCommands.h"
+
+VescCommands(HardwareSerial &ser)
+    : ser_(ser)
+{
+    ser_.begin(115200);
+    inBufPtr_ = 0;
+    mode_ = ModeNull;
+    modeValue_ = 0;
+    newValues_ = false;
+    memset(&vals_, 0, sizeof(vals_));
+    lastPoll_ = 0;
+    lastCmd_ = 0;
+}
+
+void VescCommands::setHandbrake(uint16_t h1000) {
+    modeValue_ = h1000;
+    mode_ = ModeHandbrake;
+}
+
+void VescCommands::setRPM(int32_t rpm) {
+    modeValue_ = rpm;
+    mode_ = ModeRPM;
+}
+
+void VescCommands::setCurrentBrake(int32_t mA) {
+    modeValue_ = mA;
+    mode_ = ModeCurrentBrake;
+}
+
+void VescCommands::setCurrent(int32_t mA) {
+    modeValue_ = mA;
+    mode_ = ModeCurrent;
+}
+
+void VescCommands::setDuty(int32_t d100000) {
+    modeValue_ = d100000;
+    mode_ = ModeDuty;
+}
+
+void VescCommands::setNull() {
+    mode_ = ModeNull;
+}
+
+bool VescCommands::gotNewValues() {
+    bool r = newValues_;
+    if (r) {
+        newValues_ = false;
+    }
+    return r;
+}
+
+void VescCommands::poll(uint32_t now) {
+    if (lastPoll_ == 0 || (now - lastPoll_ >= 200)) {
+        //  only send the poll command with 5 ms spacing to cmd
+        if (now - lastCmd_ >= 5) {
+            //  send poll command
+            pure_cmd(COMM_GET_VALUES);
+            lastPoll_ = now;
+        }
+    } else if ((lastCmd_ == 0 || (now - lastCmd_ >= 10)) && (now - lastPoll_ >= 5)) {
+        //  only send regular command with 5 ms spacing to poll
+        lastCmd_ = now;
+        switch (mode_) {
+            case ModeHandbrake:
+                int32_cmd(COMM_SET_HANDBRAKE, (int32_t)modeValue_);
+                break;
+            case ModeRPM:
+                int32_cmd(COMM_SET_RPM, (int32_t)modeValue_);
+                break;
+            case ModeCurrentBrake:
+                int32_cmd(COMM_SET_CURRENT_BRAKE, (int32_t)modeValue_);
+                break;
+            case ModeCurrent:
+                int32_cmd(COMM_SET_CURRENT, (int32_t)modeValue_);
+                break;
+            case ModeDuty:
+                int32_cmd(COMM_SET_DUTY, (int32_t)modeValue_);
+                break;
+            default:
+                //  send nothing
+                break;
+        }
+    }
+    while (ser_.available()) {
+        uint8_t b = ser_.read();
+        if (inBufPtr_ == 0) {
+            if (b != 2 && b != 3) {
+                //  skip this byte
+                continue;
+            }
+            inBuf_[0] = b;
+            inBufPtr_ = 1;
+        } else {
+            inBuf_[inBufPtr_++] = b;
+            if (inBuf_[0] == 2) {
+                if (inBufPtr_ == 2) {
+                    if (inBufPtr_[1] > sizeof(inBuf_) - 2 - 3) {
+                        inBufPtr_ = 0;
+                        continue;
+                    }
+                } else if (inBufPtr_ == 2 + inBufPtr_[1] + 3) {
+                    decode_packet(&inBuf_[3], ((uint16_t)inBuf_[1] << 8) + inBuf_[2]);
+                    inBufPtr_ = 0;
+                }
+            } else { // inBufPtr_[0] == 3
+                if (inBufPtr_ == 3) {
+                    if (((uint16_t)inBuf_[1] << 8) + inBuf_[2] > sizeof(inBuf_) - 3 - 3) {
+                        inBufPtr_ = 0;
+                        continue;
+                    }
+                } else if (inBufPtr_ == 3 + ((uint16_t)inBuf_[1] << 8) + inBuf_[2] + 3) {
+                    decode_packet(&inBuf_[3], ((uint16_t)inBuf_[1] << 8) + inBuf_[2]);
+                    inBufPtr_ = 0;
+                }
+            }
+        }
+    }
+}
+
+static int32_t get_be_i32(unsigned char const *&ptr) {
+    uint32_t r = ((uint32_t)ptr[0] << 24) + ((uint32_t)ptr[1] << 16) +
+        ((uint32_t)ptr[2] << 8) + ((uint32_t)ptr[3]);
+    ptr += 4;
+    return (int32_t)r;
+}
+
+static float get_be_f32(unsigned char const *&ptr, float scale) {
+    int32_t i = get_be_i32(ptr);
+    return i / scale;
+}
+
+static int16_t get_be_i16(unsigned char const *&ptr) {
+    uint16_t r = ((uint16_t)ptr[0] << 8) + ((uint16_t)ptr[1]);
+    ptr += 2;
+    return (int16_t)r;
+}
+
+static float get_be_f16(unsigned char const *&ptr, float scale) {
+    int16_t i = get_be_i16(ptr);
+    return i / scale;
+}
+
+static void put_be_i32(unsigned char const *&ptr, int32_t value) {
+    ptr[0] = (uint8_t)((uint32_t)value >> 24);
+    ptr[1] = (uint8_t)((uint32_t)value >> 16);
+    ptr[2] = (uint8_t)((uint32_t)value >> 8);
+    ptr[3] = (uint8_t)((uint32_t)value);
+    ptr += 4;
+}
+
+static void put_be_u16(unsigned char const *&ptr, uint16_t value) {
+    ptr[0] = (uint8_t)((uint32_t)value >> 8);
+    ptr[1] = (uint8_t)((uint32_t)value);
+    ptr += 2;
+}
+
+
+bool VescCommands::decode_packet(unsigned char const *ptr, uint16_t size) {
+    if (size < 3) {
+        return false;
+    }
+    if (ptr[size-1] != 3) {
+        return false;
+    }
+    uint16_t crc = crc16(ptr, size-3);
+    uint16_t bufCrc = ((uint16_t)ptr[size-3] << 8) + ptr[size-2];
+    if (crc != bufCrc) {
+        return false;
+    }
+    if (size > 3) {
+        switch (ptr[0]) {
+            case COMM_GET_VALUES:
+                if (size >= 1+12*4+4*2+1) {
+                    vals_.tempFET_10 = get_be_f16(ptr, 10.0f);
+                    vals_.tempMotor_10 = get_be_f16(ptr, 10.0f);
+                    vals_.avgMotorCurrent_100 = get_be_f32(ptr, 100.0f);
+                    vals_.avgInputCurrent_100 = get_be_f32(ptr, 100.0f);
+                    vals_.avgId_100 = get_be_f32(ptr, 100.0f);
+                    vals_.avgIq_100 = get_be_f32(ptr, 100.0f);
+                    vals_.dutyCycle_1000 = get_be_f16(ptr, 1000.0f);
+                    vals_.rpm_1 = get_be_f32(ptr, 1.0f);
+                    vals_.inputVoltage_10 = get_be_f16(ptr, 10.0f);
+                    vals_.ampHours_10000 = get_be_f32(ptr, 10000.0f);
+                    vals_.ampHoursCharged_10000 = get_be_f32(ptr, 10000.0f);
+                    vals_.wattHours_10000 = get_be_f32(ptr, 10000.0f);
+                    vals_.wattHoursCharged_10000 = get_be_f32(ptr, 10000.0f);
+                    vals_.tachometer = get_be_i32(ptr);
+                    vals_.tachometerAbs = get_be_i32(ptr);
+                    vals_.fault = *ptr++;
+                    vals_.pidPos_1000000 = get_be_f32(ptr, 1000000.0f);
+                    return true;
+                }
+            default:
+                return false;
+        }
+    }
+}
+
+void VescCommands::pure_cmd(uint8_t cmd) {
+    unsigned char cmd[6];
+    cmd[0] = 2;
+    cmd[1] = 1;
+    cmd[2] = cmd;
+    unsigned char const *p = &cmd[3];
+    put_be_u16(p, crc16(&cmd[2], 1));
+    cmd[5] = 3;
+    ser_.write(6, cmd);
+}
+
+void VescCommands::int32_cmd(uint8_t cmd, int32_t val) {
+    unsigned char cmd[10];
+    cmd[0] = 2;
+    cmd[1] = 5;
+    cmd[2] = cmd;
+    unsigned char const *p = &cmd[3];
+    put_be_i32(p, val);
+    put_be_u16(p, crc16(&cmd[2], 1));
+    cmd[9] = 3;
+    ser_.write(10, cmd);
+}
+
+
---
+++ teensy_hat_firmware/VescCommands.h
@@ -0,0 +1,91 @@
+#if !defined(VescCommands_h)
+#define VescCommands_h
+
+/* You will need to turn on the UART app in the 
+ * VESC BLDC tool for this to work. Set baud rate 
+ * to 115200.
+ */
+class VescCommands {
+    public:
+        VescCommands(HardwareSerial &ser);
+
+        /* Set up the communication to continually send 
+         * some command to the ESC. Whatever the last-set 
+         * command is, will be sent when you poll(), about 
+         * every 10 milliseconds (if you poll at least 
+         * that often.)
+         */
+        void setHandbrake(uint16_t h1000);
+        void setRPM(int32_t rpm);
+        void setCurrentBrake(int32_t mA);
+        void setCurrent(int32_t mA);
+        void setDuty(int32_t d100000);
+        /* setNull() will turn off sending commands. It 
+         * will still send get-status requests. The ESC 
+         * will go into timeout even when you poll for the 
+         * status, if no command is sent.
+         */
+        void setNull();
+
+        /* poll() will send whatever the last set command is, 
+         * and will check whether there is a new status 
+         * received from the ESC.
+         */
+        void poll(uint32_t now);
+
+        /* If a new status is received, this function will 
+         * return true, once per new received status. You 
+         * can read the actual data from the vals_ member.
+         */
+        bool gotNewValues();
+
+        /* poke at internals if you wish */
+        HardwareSerial &ser_;
+        uint32_t lastCmd_;
+        uint32_t lastPoll_;
+
+        struct Values {
+            float tempFET_10;
+            float tempMotor_10;
+            float avgMotorCurrent_100;
+            float avgInputCurrent_100;
+            float avgId_100;
+            float avgIq_100;
+            float dutyCycle_1000;
+            float rpm_1;
+            float inputVoltage_10;
+            float ampHours_10000;
+            float ampHoursCharged_10000;
+            float wattHours_10000;
+            float wattHoursCharged_10000;
+            int32_t tachometer;
+            int32_t tachometerAbs;
+            uint8_t fault;
+            float pidPos_1000000;
+        };
+        /* These are the values reported by the ESC.
+         */
+        Values vals_;
+        bool newValues_;
+
+        enum Mode {
+            ModeNone = 0,
+            ModeHandbrake,
+            ModeRPM,
+            ModeCurrentBrake,
+            ModeCurrent,
+            ModeDuty
+        };
+        Mode mode_;
+        float modeValue_;
+
+    private:
+        bool decode_packet(unsigned char const *ptr, uint16_t size);
+        void pure_cmd(uint8_t cmd);
+        void int32_cmd(uint8_t cmd, int32_t val);
+
+        uint8_t inBuf_[128];
+        uint8_t inBufPtr_;
+};
+
+#endif  //  VescCommands_h
